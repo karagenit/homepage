ba098b0106980d1f4b59e136535b0da38f13b8c4
Caleb Smith <karagenit@outlook.com>
2018-03-23 11:34:39 -0400

    Add Bootstrap Container & Card

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Card extends Component {
+  render() {
+    return (
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">
+              { this.props.title }
+            </h5>
+          </div>
+        </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="container">
+        <Card title="Github" />
       </div>
     );
   }
