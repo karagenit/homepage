6747510d426fc9ec12c8a53231c1be00e418edb3
Caleb Smith <karagenit@outlook.com>
2018-03-23 13:03:33 -0400

    Add Links

--- src/data.js
+++ src/data.js
@@ -3,22 +3,52 @@ var data = [
         name: 'Github',
         links: [
             {
-                name: 'Home',
-                url: 'https://github.com'
+                name: 'Dashboard',
+                url: 'https://github.com/'
             },
             {
-                name: 'Test',
-                url: '#'
+                name: 'New Repository',
+                url: 'https://github.com/new'
             },
             {
-                name: 'Another',
-                url: '#'
+                name: 'Profile',
+                url: 'https://github.com/karagenit'
+            },
+            {
+                name: 'All Repos',
+                url: 'https://github.com/karagenit?tab=repositories'
+            },
+            {
+                name: 'Gist',
+                url: 'https://gist.github.com/'
+            }
+        ]
+    },
+    {
+        name: 'FIRST',
+        links: [
+            {
+                name: 'Chief Delphi',
+                url: 'http://chiefdelphi.com'
+            },
+            {
+                name: 'TechHOUNDS',
+                url: 'http://techhounds.com'
             }
         ]
     },
     {
         name: 'Google',
-        links: []
+        links: [
+            {
+                name: 'Drive',
+                url: 'https://drive.google.com'
+            },
+            {
+                name: 'Calendar',
+                url: 'https://calendar.google.com'
+            }
+        ]
     }
 ]
 
