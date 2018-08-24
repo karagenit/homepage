7014073eb6a201b7ac953f471b8045c037e38dab
Caleb Smith <karagenit@outlook.com>
2018-08-24 12:16:47 -0400

    Add Links

--- src/data.js
+++ src/data.js
@@ -1,227 +1,235 @@
 var data = [
-    {
-        name: 'Github',
-        links: [
-            {
-                name: 'Dashboard',
-                url: 'https://github.com/'
-            },
-            {
-                name: 'New Repository',
-                url: 'https://github.com/new'
-            },
-            {
-                name: 'Profile',
-                url: 'https://github.com/karagenit'
-            },
-            {
-                name: 'All Repos',
-                url: 'https://github.com/karagenit?tab=repositories'
-            },
-            {
-                name: 'Gist',
-                url: 'https://gist.github.com/'
-            },
-            {
-                name: 'FRC 868',
-                url: 'https://github.com/frc868'
-            }
-        ],
-        inputs: [
-            {
-                prefix: 'karagenit/',
-                placeholder: 'repository',
-                url: 'https://github.com/karagenit/'
-            }
-        ]
-    },
-    {
-        name: 'Code Resources',
-        links: [
-            {
-                name: 'Stack Overflow',
-                url: 'https://stackoverflow.com'
-            },
-            {
-                name: 'Ruby Gems',
-                url: 'https://rubygems.org/profiles/karagenit'
-            },
-            {
-                name: 'JS Fiddle',
-                url: 'https://jsfiddle.net'
-            },
-            {
-                name: 'Sourcerer',
-                url: 'https://sourcerer.io'
-            }
-        ]
-    },
-    {
-        name: 'Code Challenges',
-        links: [
-            {
-                name: 'Hacker Rank',
-                url: 'https://hackerrank.com'
-            },
-            {
-                name: 'Hacker One',
-                url: 'https://hackerone.com'
-            },
-            {
-                name: 'Code Chef',
-                url: 'https://codechef.com'
-            },
-            {
-                name: 'Top Coder',
-                url: 'https://topcoder.com'
-            },
-            {
-                name: 'Kaggle',
-                url: 'https://kaggle.com'
-            },
-            {
-                name: 'CTF Time',
-                url: 'https://ctftime.org'
-            },
-            {
-                name: 'Produce Hunt',
-                url: 'https://producthunt.com/topics/open-source'
-            },
-            {
-                name: 'Over the Wire',
-                url: 'https://overthewire.org'
-            }
-        ]
-    },
-    {
-        name: 'FIRST',
-        links: [
-            {
-                name: 'Chief Delphi',
-                url: 'http://chiefdelphi.com'
-            },
-            {
-                name: 'TechHOUNDS',
-                url: 'http://techhounds.com'
-            },
-            {
-                name: 'FIRST Updates Now',
-                url: 'http://www.firstupdatesnow.com'
-            },
-            {
-                name: 'Indiana Robotics YouTube',
-                url: 'https://www.youtube.com/user/IndianaRobotics/playlists'
-            },
-            {
-                name: 'The Blue Alliance',
-                url: 'https://www.thebluealliance.com'
-            }
-        ]
-    },
-    {
-        name: 'Google',
-        links: [
-            {
-                name: 'Drive',
-                url: 'https://drive.google.com'
-            },
-            {
-                name: 'Calendar',
-                url: 'https://calendar.google.com'
-            },
-            {
-                name: 'Hangouts',
-                url: 'https://hangouts.google.com'
-            },
-            {
-                name: 'Gmail',
-                url: 'https://gmail.com'
-            }
-        ]
-    },
-    {
-        name: 'Other',
-        links: [
-            {
-                name: 'Wikipedia',
-                url: 'https://en.wikipedia.org'
-            },
-            {
-                name: 'Reddit',
-                url: 'https://reddit.com'
-            },
-            {
-                name: 'Outlook',
-                url: 'https://outlook.com'
-            }
-        ]
-    },
-    {
-        name: 'School',
-        links: [
-            {
-                name: 'MyPurdue',
-                url: 'https://wl.mypurdue.purdue.edu'
-            },
-            {
-                name: 'CS Opportunities',
-                url: 'https://www.cs.purdue.edu/corporate/opportunity-update/index.html'
-            },
-            {
-                name: 'CS 240',
-                url: 'http://courses.cs.purdue.edu/doku.php?id=cs24000:fall18:start'
-            },
-            {
-                name: 'CS 182',
-                url: 'https://www.cs.purdue.edu/homes/spa/cs182.html'
-            },
-            {
-                name: 'MA 161',
-                url: 'http://www.math.purdue.edu/academic/courses/coursepage?subject=MA&course=16100I'
-            }
-        ]
-    },
-    {
-        name: 'News/Blogs',
-        links: [
-            {
-                name: 'Hacker News',
-                url: 'https://news.ycombinator.com'
-            },
-            {
-                name: 'EEV Blog',
-                url: 'https://eevblog.com'
-            },
-            {
-                name: 'Hacker Noon',
-                url: 'https://hackernoon.com'
-            },
-            {
-                name: 'Basecamp Medium',
-                url: 'https://m.signalvnoise.com'
-            },
-            {
-                name: 'Hackaday',
-                url: 'https://hackaday.com'
-            },
-            {
-                name: 'Dev',
-                url: 'https://dev.to'
-            },
-            {
-                name: 'Medium',
-                url: 'https://medium.com/topic/technology'
-            },
-            {
-                name: 'n-gate',
-                url: 'https://n-gate.com'
-            },
-            {
-                name: 'Google Testing Blog',
-                url: 'https://testing.googleblog.com'
-            }
-        ]
-    }
+  {
+    name: 'Github',
+    links: [
+      {
+        name: 'Dashboard',
+        url: 'https://github.com/'
+      },
+      {
+        name: 'New Repository',
+        url: 'https://github.com/new'
+      },
+      {
+        name: 'Profile',
+        url: 'https://github.com/karagenit'
+      },
+      {
+        name: 'All Repos',
+        url: 'https://github.com/karagenit?tab=repositories'
+      },
+      {
+        name: 'Gist',
+        url: 'https://gist.github.com/'
+      },
+      {
+        name: 'FRC 868',
+        url: 'https://github.com/frc868'
+      }
+    ],
+    inputs: [
+      {
+        prefix: 'karagenit/',
+        placeholder: 'repository',
+        url: 'https://github.com/karagenit/'
+      }
+    ]
+  },
+  {
+    name: 'Code Resources',
+    links: [
+      {
+        name: 'Stack Overflow',
+        url: 'https://stackoverflow.com'
+      },
+      {
+        name: 'Ruby Gems',
+        url: 'https://rubygems.org/profiles/karagenit'
+      },
+      {
+        name: 'JS Fiddle',
+        url: 'https://jsfiddle.net'
+      },
+      {
+        name: 'Sourcerer',
+        url: 'https://sourcerer.io'
+      }
+    ]
+  },
+  {
+    name: 'Code Challenges',
+    links: [
+      {
+        name: 'Hacker Rank',
+        url: 'https://hackerrank.com'
+      },
+      {
+        name: 'Hacker One',
+        url: 'https://hackerone.com'
+      },
+      {
+        name: 'Code Chef',
+        url: 'https://codechef.com'
+      },
+      {
+        name: 'Top Coder',
+        url: 'https://topcoder.com'
+      },
+      {
+        name: 'Kaggle',
+        url: 'https://kaggle.com'
+      },
+      {
+        name: 'CTF Time',
+        url: 'https://ctftime.org'
+      },
+      {
+        name: 'Produce Hunt',
+        url: 'https://producthunt.com/topics/open-source'
+      },
+      {
+        name: 'Over the Wire',
+        url: 'https://overthewire.org'
+      }
+    ]
+  },
+  {
+    name: 'FIRST',
+    links: [
+      {
+        name: 'Chief Delphi',
+        url: 'http://chiefdelphi.com'
+      },
+      {
+        name: 'TechHOUNDS',
+        url: 'http://techhounds.com'
+      },
+      {
+        name: 'FIRST Updates Now',
+        url: 'http://www.firstupdatesnow.com'
+      },
+      {
+        name: 'Indiana Robotics YouTube',
+        url: 'https://www.youtube.com/user/IndianaRobotics/playlists'
+      },
+      {
+        name: 'The Blue Alliance',
+        url: 'https://www.thebluealliance.com'
+      }
+    ]
+  },
+  {
+    name: 'Google',
+    links: [
+      {
+        name: 'Drive',
+        url: 'https://drive.google.com'
+      },
+      {
+        name: 'Calendar',
+        url: 'https://calendar.google.com'
+      },
+      {
+        name: 'Hangouts',
+        url: 'https://hangouts.google.com'
+      },
+      {
+        name: 'Gmail',
+        url: 'https://gmail.com'
+      }
+    ]
+  },
+  {
+    name: 'Other',
+    links: [
+      {
+        name: 'Wikipedia',
+        url: 'https://en.wikipedia.org'
+      },
+      {
+        name: 'Reddit',
+        url: 'https://reddit.com'
+      },
+      {
+        name: 'Outlook',
+        url: 'https://outlook.com'
+      }
+    ]
+  },
+  {
+    name: 'School',
+    links: [
+      {
+        name: 'MyPurdue',
+        url: 'https://wl.mypurdue.purdue.edu'
+      },
+      {
+        name: 'Blackboard',
+        url: 'https://mycourses.purdue.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_17_1'
+      },
+      {
+        name: 'Piazza',
+        url: 'https://piazza.com/class/jl2lfh0swha6fa?cid=10#'
+      },
+      {
+        name: 'CS Opportunities',
+        url: 'https://www.cs.purdue.edu/corporate/opportunity-update/index.html'
+      },
+      {
+        name: 'CS 240',
+        url: 'http://courses.cs.purdue.edu/doku.php?id=cs24000:fall18:start'
+      },
+      {
+        name: 'CS 182',
+        url: 'https://www.cs.purdue.edu/homes/spa/cs182.html'
+      },
+      {
+        name: 'MA 161',
+        url: 'http://www.math.purdue.edu/academic/courses/coursepage?subject=MA&course=16100I'
+      }
+    ]
+  },
+  {
+    name: 'News/Blogs',
+    links: [
+      {
+        name: 'Hacker News',
+        url: 'https://news.ycombinator.com'
+      },
+      {
+        name: 'EEV Blog',
+        url: 'https://eevblog.com'
+      },
+      {
+        name: 'Hacker Noon',
+        url: 'https://hackernoon.com'
+      },
+      {
+        name: 'Basecamp Medium',
+        url: 'https://m.signalvnoise.com'
+      },
+      {
+        name: 'Hackaday',
+        url: 'https://hackaday.com'
+      },
+      {
+        name: 'Dev',
+        url: 'https://dev.to'
+      },
+      {
+        name: 'Medium',
+        url: 'https://medium.com/topic/technology'
+      },
+      {
+        name: 'n-gate',
+        url: 'https://n-gate.com'
+      },
+      {
+        name: 'Google Testing Blog',
+        url: 'https://testing.googleblog.com'
+      }
+    ]
+  }
 ]
 
 module.exports = data
